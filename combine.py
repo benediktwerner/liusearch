e15a57b28ae520ec3e1e9b0b57b2bc591deb8704
Benedikt Werner <1benediktwerner@gmail.com>
2022-12-17 21:03:32 +0100

    Add combine.py

---
+++ combine.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import sys, os
+
+if len(sys.argv) <= 3:
+    print("Usage:", sys.argv[0], "OUTFILE <files to combine>")
+    exit(1)
+
+out = sys.argv[1]
+rest = sys.argv[2:]
+
+if os.path.exists(out):
+    print("Outpath already exists")
+    exit(2)
+
+combined = set()
+
+for fname in rest:
+    print(fname)
+    with open(fname) as f:
+        for line in f:
+            combined.add(line)
+
+print("Outputing")
+
+with open(out, "w") as of:
+    for name in sorted(combined):
+        print(name, file=of)
